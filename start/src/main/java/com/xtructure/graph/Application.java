ff9922b028c88bf850a5cd4554ed73d000123b1e
Wang_Ziren <245107137@qq.com>
2021-04-07 23:38:40 +0800

    build(config):添加redis配置

--- start/src/main/java/com/xtructure/graph/Application.java
+++ start/src/main/java/com/xtructure/graph/Application.java
@@ -1,6 +1,8 @@
 package com.xtructure.graph;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author Frank Zhang
  */
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"com.xtructure.graph","com.alibaba.cola"})
 @MapperScan("com.xtructure.graph.customer")
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("项目启动成功！");
     }
 }

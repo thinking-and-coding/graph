02c577884417b4025fa08a977d0ff946a17a9735
wangziren3 <wangziren3@jd.com>
2021-04-08 17:50:40 +0800

    build(config):调整配置文件

--- start/src/main/java/com/xtructure/graph/Application.java
+++ start/src/main/java/com/xtructure/graph/Application.java
@@ -5,14 +5,22 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
- * Spring Boot Starter
- *
- * @author Frank Zhang
+ * 项目启动类
  */
 @Slf4j
-@SpringBootApplication(scanBasePackages = {"com.xtructure.graph","com.alibaba.cola"})
+@EnableAsync
+@EnableCaching
+@SpringBootApplication(scanBasePackages = {"com.xtructure.graph", "com.alibaba.cola"})
+@PropertySource(value = {
+        "classpath:db.properties",
+        "classpath:redis.properties"
+        }, encoding = "UTF-8")
 @MapperScan("com.xtructure.graph.customer")
 public class Application implements CommandLineRunner {
 
@@ -22,6 +30,6 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        log.info("项目启动成功！");
+        log.info("项目启动成功!");
     }
 }

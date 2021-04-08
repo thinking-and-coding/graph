bb2e418e46df893f8d526e7073235a92e7b824a8
wangziren3 <wangziren3@jd.com>
2021-04-08 20:18:22 +0800

    feat(common):引入redis客户端

---
+++ graph-common/src/main/java/com/xtructure/graph/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.xtructure.graph.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * Description: // Redis配置类
+ * <p>
+ * Created by wangziren on 2021/4/8.
+ * Create time: 6:32 下午
+ */
+@Configuration
+public class RedisConfig {
+
+    /**
+     * Lettuce配置
+     *
+     * @param lettuceConnectionFactory 连接工厂
+     */
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        return redisTemplate;
+    }
+}

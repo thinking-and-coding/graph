ab4a80433f6dd34c2651f72a49888fca772d44d9
Wang_Ziren <245107137@qq.com>
2021-04-11 00:24:34 +0800

    build(config):调整redis配置

--- graph-common/src/main/java/com/xtructure/graph/config/RedisConfig.java
+++ graph-common/src/main/java/com/xtructure/graph/config/RedisConfig.java
@@ -1,22 +1,21 @@
 package com.xtructure.graph.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -33,8 +32,10 @@ import static org.springframework.data.redis.cache.RedisCacheConfiguration.defau
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
 
-    @Resource
-    private RedisConnectionFactory redisConnectionFactory;
+    private static final String DELIMITER = ".";
+
+    @Autowired
+    private LettuceConnectionFactory redisConnectionFactory;
 
     /**
      * Lettuce配置
@@ -58,21 +59,15 @@ public class RedisConfig extends CachingConfigurerSupport {
         return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfiguration).build();
     }
 
-    @Bean
-    @Override
-    public CacheResolver cacheResolver() {
-        // TODO
-        return null;
-    }
 
     @Bean(value = "mykeyGenerator")
     @Override
     public KeyGenerator keyGenerator() {
         return (o, method, objects) -> {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(o.getClass().getName()).append(".");
-            stringBuilder.append(method.getName()).append(".");
-            stringBuilder.append(Arrays.stream(objects).map(Object::toString).collect(Collectors.joining(".")));
+            stringBuilder.append(o.getClass().getName()).append(DELIMITER);
+            stringBuilder.append(method.getName()).append(DELIMITER);
+            stringBuilder.append(Arrays.stream(objects).map(Object::toString).collect(Collectors.joining(DELIMITER)));
             log.debug("|->RedisConfig.keyGenerator.stringBuilder:{}", stringBuilder.toString());
             return stringBuilder.toString();
         };
